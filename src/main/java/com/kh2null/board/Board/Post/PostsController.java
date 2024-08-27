4e76029deff8c4dc91ea9f723b6bfaa21ca821f1
pandahwang <pandahwang@naver.com>
2024-08-27 23:54:36 +0900

    .

--- src/main/java/com/kh2null/board/Board/Post/PostsController.java
+++
@@ -1,8 +0,0 @@
-package com.kh2null.board.Board.Post;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class PostController {
-
-}
--- src/main/java/com/kh2null/board/Board/Post/PostsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kh2null.board.Board.Post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
