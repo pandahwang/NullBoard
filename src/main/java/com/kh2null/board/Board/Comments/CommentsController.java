011807156a9ea3d782655cf55c5e76d8fb7b47f0
pandahwang <pandahwang@naver.com>
2024-08-29 00:21:28 +0900

    댓글 작성 구현

--- src/main/java/com/kh2null/board/Board/Comments/CommentsController.java
+++ src/main/java/com/kh2null/board/Board/Comments/CommentsController.java
@@ -1,6 +1,19 @@
 package com.kh2null.board.Board.Comments;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
+@RequiredArgsConstructor
 public class CommentsController {
+
+    private final CommentsService commentsService;
+
+    @PostMapping("/board/post/{postId}/comment")
+    public String writeComment(Comments comments) {
+        commentsService.writeComment(comments);
+        return "redirect:/board/post/{postId}";
+    }
+
 }
--- src/main/java/com/kh2null/board/Board/Comments/CommentsService.java
+++ src/main/java/com/kh2null/board/Board/Comments/CommentsService.java
@@ -1,4 +1,15 @@
 package com.kh2null.board.Board.Comments;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommentsService {
+
+    private final CommentsRepository commentsRepository;
+
+    public void writeComment(Comments comments) {
+        commentsRepository.save(comments);
+    }
 }
