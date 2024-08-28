c657bdf531a54f2a2738eaf224956bff1a164e53
pandahwang <pandahwang@naver.com>
2024-08-28 18:55:17 +0900

    글쓰기, 게시글 페이지 이동, 게시글 수정 페이지 이동, 게시글 수정 구현

--- src/main/java/com/kh2null/board/Board/Posts/PostsController.java
+++ src/main/java/com/kh2null/board/Board/Posts/PostsController.java
@@ -1,16 +1,62 @@
 package com.kh2null.board.Board.Posts;
 
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class PostsController {
 
-    @GetMapping("/posts/save")
-    public String postsSave() {
-        return "posts-save";
+    private final PostsRepository postsRepository;
+    private final PostsService postsService;
+    
+    // 글쓰기 페이지 이동
+    @GetMapping("/write-form")
+    public String writeForm(){
+        return "write-form";
+    }
+    
+    // 글쓰기
+    @PostMapping("/write-form")
+    public String write(Posts posts){
+        postsService.save(posts);
+        return "redirect:/board";
+    }
+    
+    // 게시글 페이지 이동
+    // pathvariable로 조회
+    @GetMapping("/post/{post_id}")
+    @ResponseBody
+    public Posts readPostById(@PathVariable int post_id){
+        Posts result = postsService.getPostById(post_id);
+        return result;
+    }
+
+//    // queryparameter로 조회
+//    @GetMapping("/post")
+//    @ResponseBody
+//    public Posts readPost(@RequestParam int post_id){
+//        Posts result = postsService.getPostById(post_id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));
+//        return result;
+//    }
+    
+    // 게시글 수정 페이지 이동
+    @GetMapping("/board/post/{post_id}/edit")
+    public String editPostPage(@PathVariable int post_id, Model model){
+        Posts result = postsService.getPostById(post_id);
+        return "editPost";
+    }
+    
+    // 게시글 수정
+    @PostMapping("/board/post/{post_id}/edit")
+    public String editPost(@PathVariable int post_id, Posts posts){
+        postsService.editPostById(post_id, posts);
+        return "redirect:/board";
     }
 
     @GetMapping("/posts/update")
